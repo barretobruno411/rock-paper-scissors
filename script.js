55ceedd39b6c0eda27ff930f309e668de261a617
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-05 17:05:56 -0300

    Add getComputerChoice and getPlayerChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Helo world!")
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
+};
+
+const getPlayerChoice = () => {
+    const regex = /rock|paper|Scissors/i
+    const choice = prompt("chose your weapon: ")
+    if (regex.test(choice)){
+        return choice.toLowerCase();
+    } else {
+        console.log("not valid, choice between rock, paper and scissors")
+        getPlayerChoice()
+    }
+}
+
