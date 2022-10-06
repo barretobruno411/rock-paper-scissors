faa70d02d7e08faecac2e907a53046e2de008a3d
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-06 09:32:21 -0300

    Add module gameRound

--- modules/gameRound.mjs
+++ modules/gameRound.mjs
@@ -1,3 +1,15 @@
 export const gameRound = (computer, player) => {
-    
-}
+  const computerChoice = computer();
+  const playerChoice = player();
+  if (computerChoice === playerChoice) {
+    return `Draw! ${playerChoice} and ${computerChoice} are the same`;
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    return `You win! ${playerChoice} beats ${computerChoice}.`;
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    return `You win! ${playerChoice} beats ${computerChoice}.`;
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return `You win! ${playerChoice} beats ${computerChoice}.`;
+  } else {
+    return `You lose! ${computerChoice} beats ${playerChoice}`
+  }
+};
--- script.mjs
+++ script.mjs
@@ -1,7 +1,8 @@
 import {getComputerChoice} from "./modules/ComputerChoice.mjs"
 import {getPlayerChoice} from "./modules/PlayerChoice.mjs"
+import {gameRound} from "./modules/gameRound.mjs"
 
-console.log(getComputerChoice())
+console.log(gameRound(getComputerChoice, getPlayerChoice))
 
 
 
