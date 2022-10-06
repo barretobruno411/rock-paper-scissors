9a82eb664c63fdf7500edd9110b97daaffe82c57
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-06 10:05:43 -0300

    Add game module and finished project

---
+++ modules/game.mjs
@@ -0,0 +1,17 @@
+import {getComputerChoice} from "./ComputerChoice.mjs"
+import {getPlayerChoice} from "./PlayerChoice.mjs"
+import {gameRound} from "./gameRound.mjs"
+export const game = (round) => {
+    const regex = /win/i
+    const wons = []
+    for (let i = 1; i <= 5; i++) {
+        let roundResult = gameRound(getComputerChoice,getPlayerChoice)
+        if (regex.test(roundResult)) wons.push("won")
+        console.log(roundResult)
+    }
+    if (wons.length >= 3) {
+        console.log("You won the game " + wons.length + " points")
+    } else {
+        console.log(`"You lose the game, just make ${wons.length} points!`)
+    }
+}
--- script.mjs
+++ script.mjs
@@ -1,8 +1,9 @@
 import {getComputerChoice} from "./modules/ComputerChoice.mjs"
 import {getPlayerChoice} from "./modules/PlayerChoice.mjs"
 import {gameRound} from "./modules/gameRound.mjs"
+import {game} from "./modules/game.mjs"
 
-console.log(gameRound(getComputerChoice, getPlayerChoice))
+// console.log(gameRound(getComputerChoice, getPlayerChoice))
 
-
+game();
 
